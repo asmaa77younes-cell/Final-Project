ee462519b5786ce7f01d46d3cf2e16b9d7ee9f45
Asmaa <asmaa77younes@gmail.com>
2026-07-22 21:38:08 +0300

    modifying config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,17 @@
-module.exports = {
+require("dotenv").config();
+
+if(!process.env.MONGO_URI){
+  throw new Error("MONGO_URI isn't defined in .env file")
+}
+
+if(!process.env.PORT){
+  console.log("Port not found, we will use the virtual port = 3000")
+}
+
+const config = {
   PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || "development",
   MONGO_URI: process.env.MONGO_URI,
+  NODE_ENV: process.env.NODE_ENV || "development"
 };
+
+module.exports = config;
